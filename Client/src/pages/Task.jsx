e3ba320db5d41e6bcf41b7ee350461a45802deea
Mujahid087 <mujahidmahedi10@gmail.com>
2024-12-05 15:06:09 +0530

    created the task card

--- Client/src/pages/Task.jsx
+++ Client/src/pages/Task.jsx
@@ -2,39 +2,73 @@ import React from 'react'
 import { useState } from 'react';
 import { FaList } from "react-icons/fa";
 import { MdGridView } from "react-icons/md";
-import {useParams} from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import Loader from '../Components/Loader';
-import Title from '@babel/core'
+import Title from '../Components/Title'
+import Button from '../Components/Button'
+import { IoMdAdd } from 'react-icons/io'
+import Tabs from '../Components/Tabs';
+import TaskTitle from '../Components/TaskTitle';
+import BoardView from '../Components/BoardView';
 
-const TABS=[
+const TABS = [
   {
-    title:"Board View",icon :<MdGridView/>
+    title: "Board View", icon: <MdGridView />
   },
   {
-    title:"List View",icon:<FaList/>
+    title: "List View", icon: <FaList />
   },
 ]
 
-const TASK_TYPE={
-  todo:"bg-blue-600",
-  "in progress":"bg-yellow-600",
-  completed:"bg-green-600",
+const TASK_TYPE = {
+  todo: "bg-blue-600",
+  "in progress": "bg-yellow-600",
+  completed: "bg-green-600",
 }
 const Task = () => {
 
-  const params=useParams()
+  const params = useParams()
 
-  const [selected,setSelected]=useState(0)
-  const [open,setOpen]=useState(false)
-  const [loading,setLoading]=useState(false)
-  const status=params?.status || ""
+  const [selected, setSelected] = useState(0)
+  const [open, setOpen] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const status = params?.status || ""
 
   return loading ? (<div className='py-10'>
-    <Loader/>
-  </div>):(<div className='w-full'>
+    <Loader />
+  </div>) : (<div className='w-full'>
     <div className='flex items-center justify-between mb-4 '>
-      <Title title={status ? `${status} Taks` :"Tasks"}/>
+      <Title title={status ? `${status} Taks` : "Tasks"} />
 
+      {
+        !status && <Button
+          label="Create Task"
+          icon={<IoMdAdd className="text-lg" />}
+          className="flex flex-row-reverse gap-1 items-center bg-blue-600 text-white rounded-md py-2 2xl:py-2.5"
+
+        />
+      }
+
+    </div>
+    <div>
+      <Tabs tabs={TABS} setSelected={setSelected}>
+        {!status && (
+          <div className='w-full flex justify-between gap-4 md:gap-x-12 py-4'>
+            <TaskTitle label="To Do" className={TASK_TYPE.todo} />
+            <TaskTitle label="In Progress" className={TASK_TYPE["in progress"]} />
+            <TaskTitle label="completed" className={TASK_TYPE.completed} />
+          </div>
+        )}
+
+        {selected !== 1 ? (
+          <BoardView tasks={tasks} />
+        ) : (
+          <div className='w-full'>
+            <Table tasks={tasks} />
+          </div>
+        )}
+
+      </Tabs>
     </div>
 
 
