2af3ea66145d1655b6d42b5af83bf6aaec35485d
Mujahid087 <mujahidmahedi10@gmail.com>
2024-12-11 10:58:23 +0530

    finsihed setting up the server and connected the database with the application

---
+++ server/utils/index.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+const dbConnection=async ()=>{
+    try{
+        await mongoose.connect(process.env.MONGODB_URI)
+        console.log("DB connection established")
+
+    }catch(error){
+        console.log("DB Eroor:"+error)
+    }
+}
+
+export default dbConnection
