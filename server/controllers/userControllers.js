ecd9b293c5150a8bfef7a496247661f92a50b70c
Mujahid087 <mujahidmahedi10@gmail.com>
2024-12-12 22:36:50 +0530

    ending the day on jwt token

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,8 +1,57 @@
-export const registerUser=async(req,res)=>{
-    try{
+import User from "../models/user.js";
+
+export const registerUser = async (req, res) => {
+    try {
+        const { name, email, password, isAdmin, role, title } = req.body;
+        const userExist = await User.findOne({ email })
+
+        if (userExist) {
+            return res.status(400).json({
+                status: false,
+                message: "User already exist"
+            })
+        }
+        const user = await User.create({
+            // ...req.body shortcut way by using spread operator 
+            name,
+            email,
+            password,
+            isAdmin,
+            role,
+            title,
+        })
+
+        if (user) {
+            isAdmin ? createJWT(req, user._id) : null
+            user.password = undefined
+
+            res.status(201).json(user)
+        } else {
+            return user.status(400).json({
+                status: false, message: "Invalid user data"
+            })
+        }
+
+
 
     }
-    catch(error){
-        
+    catch (error) {
+        return res
+            .status(400)
+            .json({ status: false, message: error.message })
+
     }
 }
+
+// export const registerUser = async (req, res) => {
+//     try {
+
+
+//     }
+//     catch (error) {
+//         return res
+//             .status(400)
+//             .json({ status: false, message: error.message })
+
+//     }
+// }
--- server/utils/index.js
+++ server/utils/index.js
@@ -1,12 +1,28 @@
+import jwt from "jsonwebtoken"
 import mongoose from "mongoose";
-const dbConnection=async ()=>{
-    try{
+const dbConnection = async () => {
+    try {
         await mongoose.connect(process.env.MONGODB_URI)
         console.log("DB connection established")
 
-    }catch(error){
-        console.log("DB Eroor:"+error)
+    } catch (error) {
+        console.log("DB Eroor:" + error)
     }
 }
 
+
+
 export default dbConnection
+
+
+export const createJWT = (res, userId) => {
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+        expiresIn: "1d"
+    })
+
+    res.cookie("token",token,{
+        httpOnly:true,
+        secure:process.env.NODE_ENV !== "development",
+        SameSite: Strict
+    })
+}
