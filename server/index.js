6784d0f9b8b530177c9ac85c123a5411d2aafe45
Mujahid087 <mujahidmahedi10@gmail.com>
2025-01-01 12:52:02 +0530

    creating the file

--- server/index.js
+++ server/index.js
@@ -15,12 +15,26 @@ const PORT = process.env.PORT || 5000
 
 const app=express()
 
+// app.use(cors({
+//     origin:['http://localhost:3000','http://localhost:3001'],
+//     methods:["GET","POST","PUT","DELETE"],
+//     credentials:true,
+// }))
 app.use(cors({
-    origin:['http://localhost:3000','http://localhost:3001'],
-    methods:["GET","POST","PUT","DELETE"],
-    credentials:true,
+    origin: (origin, callback) => {
+        const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001'];
+        const excludedOrigins = ['http://example.com']; // Sites you want to exclude
+        
+        if (allowedOrigins.includes(origin) && !excludedOrigins.includes(origin)) {
+            callback(null, true); // Allow the request
+        } else {
+            callback(new Error('Not allowed by CORS'), false); // Deny the request
+        }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+}));
 
-}))
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
